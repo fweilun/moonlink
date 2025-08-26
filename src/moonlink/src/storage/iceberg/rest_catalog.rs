659ff0ff3c31f312de044e13140a57a4bd448119
fweilun <william20020503@gmail.com>
2025-08-26 23:43:45 +0800

    add crud testcases for rest catalog

--- src/moonlink/src/storage/iceberg/rest_catalog.rs
+++ src/moonlink/src/storage/iceberg/rest_catalog.rs
@@ -105,3 +105,186 @@ impl Catalog for RestCatalog {
         todo!("register existing table is not supported")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use iceberg::spec::{NestedField, PrimitiveType, Schema, Type};
+    use reqwest::Client;
+
+    use super::*;
+    const REST_CATALOG_PROP_URI: &str = "uri";
+
+    #[cfg(feature = "storage-gcs")]
+    #[tokio::test]
+    async fn test_create_table() {
+        let builder = IcebergRestCatalogBuilder::default().with_client(Client::new());
+
+        let catalog = builder
+            .load(
+                "test",
+                HashMap::from([
+                    (
+                        REST_CATALOG_PROP_URI.to_string(),
+                        "http://localhost:8181".to_string(),
+                    ),
+                    ("a".to_string(), "b".to_string()),
+                ]),
+            )
+            .await;
+
+        assert!(catalog.is_ok());
+        let table_creation = TableCreation::builder()
+            .name("test1".to_string())
+            .schema(
+                Schema::builder()
+                    .with_fields(vec![
+                        NestedField::optional(1, "foo", Type::Primitive(PrimitiveType::String))
+                            .into(),
+                        NestedField::required(2, "bar", Type::Primitive(PrimitiveType::Int)).into(),
+                        NestedField::optional(3, "baz", Type::Primitive(PrimitiveType::Boolean))
+                            .into(),
+                    ])
+                    .build()
+                    .unwrap(),
+            )
+            .build();
+
+        let namespace = NamespaceIdent::new("ns1".to_string());
+        let result = catalog
+            .unwrap()
+            .create_table(&namespace, table_creation)
+            .await;
+        match result {
+            Ok(_) => {}
+            Err(e) if e.to_string().contains("The table already exists") => {}
+            Err(e) => {
+                panic!("create_table failure: {e:?}")
+            }
+        }
+
+        let builder = IcebergRestCatalogBuilder::default().with_client(Client::new());
+        let catalog = builder
+            .load(
+                "test",
+                HashMap::from([
+                    (
+                        REST_CATALOG_PROP_URI.to_string(),
+                        "http://localhost:8181".to_string(),
+                    ),
+                    ("a".to_string(), "b".to_string()),
+                ]),
+            )
+            .await;
+
+        let result = catalog
+            .unwrap()
+            .table_exists(&TableIdent::new(
+                NamespaceIdent::new("ns1".to_string()),
+                "test1".to_string(),
+            ))
+            .await;
+        match result {
+            Ok(_) => {}
+            Err(e) => {
+                panic!("table_exists error: {e:?}");
+            }
+        }
+    }
+
+    #[cfg(feature = "storage-gcs")]
+    #[tokio::test]
+    async fn test_drop_table() {
+        let builder = IcebergRestCatalogBuilder::default().with_client(Client::new());
+        let catalog = builder
+            .load(
+                "test",
+                HashMap::from([
+                    (
+                        REST_CATALOG_PROP_URI.to_string(),
+                        "http://localhost:8181".to_string(),
+                    ),
+                    ("a".to_string(), "b".to_string()),
+                ]),
+            )
+            .await;
+        assert!(catalog.is_ok());
+
+        let result = catalog
+            .unwrap()
+            .drop_table(&TableIdent::new(
+                NamespaceIdent::new("ns1".to_string()),
+                "table1".to_string(),
+            ))
+            .await;
+        match result {
+            Ok(_) => {}
+            Err(e)
+                if e.to_string()
+                    .contains("Tried to drop a table that does not exist") => {}
+            Err(e) => {
+                panic!("drop_table failure: {e:?}")
+            }
+        }
+    }
+
+    #[cfg(feature = "storage-gcs")]
+    #[tokio::test]
+    async fn test_load_table() {
+        let builder = IcebergRestCatalogBuilder::default().with_client(Client::new());
+        let catalog = builder
+            .load(
+                "test",
+                HashMap::from([
+                    (
+                        REST_CATALOG_PROP_URI.to_string(),
+                        "http://localhost:8181".to_string(),
+                    ),
+                    ("a".to_string(), "b".to_string()),
+                ]),
+            )
+            .await;
+
+        assert!(catalog.is_ok());
+        let result = catalog
+            .unwrap()
+            .load_table(&TableIdent::new(
+                NamespaceIdent::new("ns1".to_string()),
+                "table1".to_string(),
+            ))
+            .await;
+        match result {
+            Ok(_) => {}
+            Err(e) if e.to_string().contains("does not exist") => {}
+            Err(e) => {
+                panic!("load table error: {e:?}");
+            }
+        }
+    }
+
+    #[cfg(feature = "storage-gcs")]
+    #[tokio::test]
+    async fn test_table_exists() {
+        let builder = IcebergRestCatalogBuilder::default().with_client(Client::new());
+        let catalog = builder
+            .load(
+                "test",
+                HashMap::from([
+                    (
+                        REST_CATALOG_PROP_URI.to_string(),
+                        "http://localhost:8181".to_string(),
+                    ),
+                    ("a".to_string(), "b".to_string()),
+                ]),
+            )
+            .await;
+        assert!(catalog.is_ok());
+        assert!(catalog
+            .unwrap()
+            .table_exists(&TableIdent::new(
+                NamespaceIdent::new("ns1".to_string()),
+                "table1".to_string(),
+            ))
+            .await
+            .unwrap());
+    }
+}
